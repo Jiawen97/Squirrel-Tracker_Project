92599c86a4cec270011a7bd13986f63c4aa30c99
Joyce Fei <yf2515@columbia.edu>
2019-12-06 01:59:21 +0000

    Delete the delete, revise the views

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name='sightings'
 urlpatterns = [
         path('',views.index,name="index"),
-        path('<Unique_Squirrel_ID>/update/',views.update,name="update"),
+        path('<Unique_Squirrel_ID>/',views.update,name="update"),
         path('add/',views.add,name="add"),
-        path('<Unique_Squirrel_ID>/delete/',views.delete,name="delete"),
         path('stats/',views.stats,name="stats"),
         path('<Unique_Squirrel_ID>/details/',views.details,name="details"),
+
 
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -13,37 +13,6 @@ def index(request):
         raise Http404("Squirrel does not exist!")
     return render(request,'sightings/index.html',context)
 
-def update(request,Unique_Squirrel_ID):
-    squirrel = sightings.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
-    if request.method=="POST":
-        squirrel.Longitude=request.POST['longitude']
-        squirrel.Latitude=request.POST['latitude']
-        squirrel.Shift=request.POST['shift']
-        squirrel.Date=request.POST['date']           
-        squirrel.Age=request.POST['age']
-        squirrel.Primary_Fur_Color=request.POST['primary_fur_color']
-        squirrel.Location=request.POST['location']
-        squirrel.Specific_Location=request.POST['specific_location']
-        squirrel.Running=request.POST['running']
-        squirrel.Chasing=request.POST['chasing']
-        squirrel.Climbing=request.POST['climbing']
-        squirrel.Eating=request.POST['eating']
-        squirrel.Foraging=request.POST['foraging']
-        squirrel.Other_activities=request.POST['other_activities']
-        squirrel.Kuks=request.POST['kuks']
-        squirrel.Quaas=request.POST['quaas']
-        squirrel.Moans=request.POST['moans']
-        squirrel.Tail_flags=request.POST['tail_flags']
-        squirrel.Tail_twitches=request.POST['tail_twitches']
-        squirrel.Approaches=request.POST['approaches']
-        squirrel.Indifferent=request.POST['indifferent']
-        squirrel.Runs_from=request.POST['runs_from']
-        squirrel.save()
-        context={'squirrel':squirrel,}
-        return render(request,'sightings/details.html',context)
-    else:
-        context={'squirrel':squirrel,}
-        return render(request,'sightings/update.html',context)
 
 def add(request):
     if request.method=="POST":
@@ -75,33 +44,58 @@ def add(request):
             squirrel.save()
             context={'sightings':sightings.objects.all(),}
             return render(request,'sightings/index.html',context)
-    else:
+    elif request.method=="GET":
             return render(request,'sightings/add.html')
-
-def delete(request,Unique_Squirrel_ID):
-    squirrel = sightings.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID) 
-    if request.method =="POST":
-        squirrel.delete()
-        return redirect('sightings:index')
-    return render(request,'sightings/delete.html')
-    
-def stats(request):
-    sightings_stats1=sightings.objects.all().count()
-    sightings_stats2=sightings.objects.filter(Primary_fur_color='Black').count()
-    sightings_stats3=sightings.objects.filter(Primary_fur_color='Gray').count()
-    sightings_stats4=sightings.objects.filter(Age='Adult').count()
-    sightings_stats5=sightings.objects.filter(Age='Juvenile').count()
-    context={
-            'sightings_stats1':sightings_stats1,
-            'sightings_stats2':sightings_stats2,
-            'sightings_stats3':sightings_stats3,
-            'sightings_stats3':sightings_stats3,
-            'sightings_stats4':sightings_stats4,
-            'sightings_stats5':sightings_stats5,
-            }
-    return render(request, 'sightings/stats.html', context)
 
 def details(request,Unique_Squirrel_ID):
     squirrel = sightings.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
     context = {'squirrel':squirrel,}
     return render(request,'sightings/details.html',context)
+
+def stats(request):
+    sightings_stats1=sightings.objects.all().count()
+    sightings_stats2=sightings.objects.filter(Primary_Fur_Color='Black').count()
+    sightings_stats3=sightings.objects.filter(Running='True').count()
+    sightings_stats4=sightings.objects.filter(Age='Adult').count()
+    sightings_stats5=sightings.objects.filter(Age='Juvenile').count()
+    context={
+            'Number of all the sightings':sightings_stats1,
+            'Number of black primary fur color sightings':sightings_stats2,
+            'Number of running sightings':sightings_stats3,
+            'Number of adult sightings':sightings_stats4,
+            'Number of juvenile sightings':sightings_stats5,
+            }
+    return render(request, 'sightings/stats.html', context)
+
+def update(request,Unique_Squirrel_ID):
+    squirrel = sightings.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
+    if request.method=="POST":
+        squirrel.Longitude=request.POST['longitude']
+        squirrel.Latitude=request.POST['latitude']
+        squirrel.Shift=request.POST['shift']
+        squirrel.Date=request.POST['date']
+        squirrel.Age=request.POST['age']
+        squirrel.Primary_Fur_Color=request.POST['primary_fur_color']
+        squirrel.Location=request.POST['location']
+        squirrel.Specific_Location=request.POST['specific_location']
+        squirrel.Running=request.POST['running']
+        squirrel.Chasing=request.POST['chasing']
+        squirrel.Climbing=request.POST['climbing']
+        squirrel.Eating=request.POST['eating']
+        squirrel.Foraging=request.POST['foraging']
+        squirrel.Other_activities=request.POST['other_activities']
+        squirrel.Kuks=request.POST['kuks']
+        squirrel.Quaas=request.POST['quaas']
+        squirrel.Moans=request.POST['moans']
+        squirrel.Tail_flags=request.POST['tail_flags']
+        squirrel.Tail_twitches=request.POST['tail_twitches']
+        squirrel.Approaches=request.POST['approaches']
+        squirrel.Indifferent=request.POST['indifferent']
+        squirrel.Runs_from=request.POST['runs_from']
+        squirrel.save()
+        context={'squirrel':squirrel,}
+        return render(request,'sightings/details.html',context)
+    elif request.method=="GET":
+        context={'squirrel':squirrel,}
+        return render(request,'sightings/update.html',context)
+
